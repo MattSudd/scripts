e3c6148b5e9c8a2fb7081a6ad671f294757374d1
MattSudd <mattsuddock@gmail.com>
2019-11-04 10:20:53 +1000

    update to makemunki.sh

--- makemunki.sh
+++ makemunki.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# todo: 
+# todo:
 # ask for git location and where files should be put.
 # this script expects the munki repo to be in ~/Documents/git/munki if your repo is elsewere please update the command accordingly
 
 
 user=`stat -f "%Su" /dev/console`
-outputdir=/Users/$user/Desktop/test/munki
+outputdir=/Users/$user/Desktop/munki
 outputdir_boot=$outputdir/bootstrap
 outputdir_update=$outputdir/update
 echo $user
@@ -14,14 +14,14 @@ echo $outputdir
 
 # if output dir doesn't exist, create it
 if [ ! -d "$outputdir" ]; then
-	echo "output directory missing - creating"
-	mkdir "$outputdir"
-	mkdir "$outputdir_boot"
-	mkdir "$outputdir_update"
+        echo "output directory missing - creating"
+        mkdir "$outputdir"
+        mkdir "$outputdir_boot"
+        mkdir "$outputdir_update"
 fi
 
 cd ~/Documents/git/munki
-git pull 
+git pull
 #git clone https://github.com/munki/munki.git
 cd ~/Documents/git/munki
 
@@ -31,4 +31,3 @@ echo "Making Munki with Bootstrap switch. Saving bootstrap to ~/Desktop/munki/bo
 
 echo "Making Munki update. Saving update to ~/Desktop/munki/update"
 /Users/$user/Documents/git/munki/code/tools/make_munki_mpkg_DEP.sh -i "com.googlecode.munki" -o $outputdir_update -s "Developer ID Installer: Black Milk Clothing Pty Ltd (USKFFV8BKR)"
-
